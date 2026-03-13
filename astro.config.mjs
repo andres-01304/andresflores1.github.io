cb984fc0884aae6336264373e8e71118cbd3eafb
andres-01304 <andres.flores.original@gmail.com>
2026-03-12 19:08:02 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kpab.github.io',
+  site: 'https://andres-01304.github.io',
   base: '/astro-darkness',
   trailingSlash: 'always',
   integrations: [mdx(), sitemap()],
